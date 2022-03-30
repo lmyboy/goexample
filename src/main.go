beb12c2c4a03665097cae5416f09e623cdaf5d9b
lmyboy <lucasldc1@gmail.com>
2022-03-30 17:19:33 -0300

    add: udemy url comment

--- src/main.go
+++ src/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+/*
+last lesson: https://www.udemy.com/course/learn-how-to-code/learn/lecture/11921976#overview
+*/
 func main() {
 	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
 }
